5fc6a08f187c4cc2730cf1aa147460d7a11c44b6
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-08-13 13:06:09 +0500

    Add function randomer

---
+++ User/others.py
@@ -0,0 +1,14 @@
+import random
+
+from working_with_file import read_all_data
+
+
+def randomer(filename: str, subject: str):
+    data = read_all_data(filename)
+    if subject == "username":
+        lis = [x[subject] for x in data["users"]]
+        lis2 = [x["password"] for x in data["users"]]
+        random_username = random.randint(1, 10000)
+        while random_username in lis or random_username in lis2:
+            random_username = random.randint(1, 10000)
+        return random_username
