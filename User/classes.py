c7be58bdd050c6f30280c93eb095e4783cee3cb2
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-08-13 12:14:44 +0500

    Add Class Group

--- User/classes.py
+++ User/classes.py
@@ -31,3 +31,18 @@ class User:
     def __str__(self):
         return (f"ID: {self.id_of}, User: {self.full_name}, Email: {self.email}, Phone: {self.phone},"
                 f"Age: {self.age}, Gender: {self.gender}, Status: {self.status}, Username: {self.username}")
+
+
+class Group:
+    def __init__(self, name: str, teacher_id: int, max_student: int,
+                 start_time: datetime, end_time: datetime, status: bool):
+        self.name: str = name
+        self.teacher_id: int = teacher_id
+        self.max_student: int = max_student
+        self.start_time: datetime = start_time
+        self.end_time: datetime = end_time
+        self.status: bool = status
+
+    def __str__(self):
+        return (f"Name: {self.name}, Teacher ID: {self.teacher_id}, Max Student: {self.max_student},"
+                f"Start Time: {self.start_time}, End Time: {self.end_time}, Status: {self.status}")
