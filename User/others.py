aa83cd8fc9cc56818a8d1fcf00cdc5cce65b602e
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-08-14 16:13:26 +0500

    Add func smtp operations

--- User/others.py
+++ User/others.py
@@ -40,3 +40,21 @@ def dry1(file_name):
     username: int = randomer(file_name, "username")
     password: int = randomer(file_name, "password")
     return username, password, id_student, data
+
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+smtp_sender = 'egamberdiyevabdurahim@gmail.com'
+smtp_password = 'yctu zenu eewo drdc'
+
+
+def send_mail(to_user, subject, message):
+    email = f"Subject: {subject}\n\n{message}"
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()
+        server.login(smtp_sender, smtp_password)
+        server.sendmail(smtp_sender, to_user, email)
+        server.quit()
+    except smtplib.SMTPException as e:
+        print(f"Failed {e}")
