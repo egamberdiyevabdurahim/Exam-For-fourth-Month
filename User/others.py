08af1399b932b4db222a95e688d9011aebf4a4e5
egamberdiyevabdurahim <egamberdiyevabdurahim@gmail.com>
2024-08-14 16:11:37 +0500

    Add func randomer for username

--- User/others.py
+++ User/others.py
@@ -1,29 +1,26 @@
 import random
+import smtplib
 
-from working_with_file import read_all_data
+from .working_with_file import read_all_data
 
 
 def randomer(filename: str, subject: str):
     data = read_all_data(filename)
-    if subject == "username":
-        lis = [x[subject] for x in data["users"]]
-        lis2 = [x["password"] for x in data["users"]]
-        random_username = random.randint(1, 10000)
-        while random_username in lis or random_username in lis2:
-            random_username = random.randint(1, 10000)
-        return random_username
-
-    elif subject == "password":
-        lis = [x[subject] for x in data["users"]]
-        lis2 = [x["username"] for x in data["users"]]
-        random_password = random.randint(1, 10000)
-        while random_password in lis or random_password in lis2:
-            random_password = random.randint(1, 10000)
-        return random_password
-
-    elif subject == "verification":
+    is_available = False
+    if subject == "verification":
         lis = [x["code"] for x in data["codes"]]
         random_code = random.randint(1, 1000)
         while random_code in lis:
             random_code = random.randint(1, 1000)
         return random_code
+
+    elif subject == "username":
+        is_available = True
+        lis = [x[subject] for x in data["users"]]
+        lis2 = [x["password"] for x in data["users"]]
+
+    if is_available:
+        random_generation = random.randint(1, 10000)
+        while random_generation in lis or random_generation in lis2:
+            random_generation = random.randint(1, 10000)
+        return random_generation
